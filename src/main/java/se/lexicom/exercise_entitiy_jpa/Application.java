d085543e542f2d5085c8daf925128b461ac8577a
Mohsen Forouzmand <career.forouzmand@gmail.com>
2022-09-08 12:33:03 +0200

    Initial commit - changing the pom-H2 dependency from real to test - creating two properties file - inserting the connection requirements into them - checking the dataBase status

---
+++ src/main/java/se/lexicom/exercise_entitiy_jpa/Application.java
@@ -0,0 +1,13 @@
+package se.lexicom.exercise_entitiy_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicom/exercise_entitiy_jpa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicom.exercise_entitiy_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
